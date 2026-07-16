09000cb51ed5227a6b17791b350ed5a5790f2515
ftjana-19 <ftjanajd@gmail.com>
2026-07-16 12:44:24 +0530

    Create Day - 1 Leet code problem (9) : Palindrome Number .java

---
+++ Day - 1 Leet code problem (9) : Palindrome Number .java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int r=0;
+        int a = x;
+        if (x<0)
+            return false;
+        
+        while(x!=0){
+            
+            r = (r*10)+(x%10);
+            x=x/10;
+
+        }
+        return a==r;
+    } 
+        
+    
+     
+        
+        
+        
+    
+}
